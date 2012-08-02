1edb25a9df701180d853b03f24bcd216cbd9a618
Menno Pietersen <info@designfromwithin.com>
2012-08-02 07:54:54 +0200

    Fix for the Orbit slider (thanks: Designforge)
    Removed duplicate Orbit slider code in app.js.

--- assets/templates/flexibility/javascripts/app.js
+++ assets/templates/flexibility/javascripts/app.js
@@ -21,15 +21,6 @@ jQuery(document).ready(function ($) {
 
 	/* Active gallery lightbox */
 	$("a[rel='fancybox']").fancybox();
-
-	/* Active slider */
-	$('#slider').orbit({
-		animation: 'fade',                  // fade, horizontal-slide, vertical-slide, horizontal-push
-		animationSpeed: 800,                // how fast animtions are
-		timer: true,                        // true or false to have the timer
-		advanceSpeed: 4000,                 // if timer is enabled, time between transitions 
-		pauseOnHover: false,                // if you hover pauses the slider
-	});
 	
 	/* TABS --------------------------------- */
 	/* Remove if you don't need :) */
